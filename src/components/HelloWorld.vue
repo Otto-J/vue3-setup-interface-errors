118895d790bfc869475dbb8c7018e59a603d5bd6
Otto <fa@ijust.cc>
2021-08-10 18:47:16 +0800

    feat: init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ name }},{{ age }}</h1>
+</template>
+
+<script setup lang="ts">
+interface IOpt {
+  name: string;
+  age: number;
+}
+
+// if use import interface from url ,error:
+// import type { IOpt } from "./types";
+const props = withDefaults(defineProps<IOpt>(), {
+  name: "xx",
+  age: 2,
+});
+</script>
---
+++ src/components/types.ts
@@ -0,0 +1,4 @@
+export interface IOpt {
+  name: string;
+  age: number;
+}
